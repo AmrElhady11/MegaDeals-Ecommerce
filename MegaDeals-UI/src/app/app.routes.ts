476fef719bb3946779039dc9c59255963fcf3274
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-06-06 04:37:55 +0300

    implementing navbar Mega-deals

--- MegaDeals-UI/src/app/app.routes.ts
+++ MegaDeals-UI/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Products } from './components/pages/products/products';
+import { MyWishlist } from './components/pages/my-wishlist/my-wishlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',pathMatch:'full', redirectTo:'products',},
+  {path:'products',component:Products},
+  {path:'wishlist',component:MyWishlist},
+];
