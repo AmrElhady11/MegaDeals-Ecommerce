9427ed550d75f4c7f01620f260ba80ffc23cc500
amrElhady11 <amr.elhady.programs@gmail.com>
2026-07-08 22:37:21 +0300

    launching megaDeals backend project

---
+++ mega-deals/src/main/java/com/megadeals/MegaDealsApplication.java
@@ -0,0 +1,13 @@
+package com.megadeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaDealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaDealsApplication.class, args);
+    }
+
+}
